052107d5524d7418c1225816d26b68341dff4831
juanstelling <juan.stelling@gmail.com>
2021-02-05 08:50:00 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IHPYVAdwIQEGoNWHBMBkjRV0ghrJ6gfIfguAJ0cdgZTJBBo1VHGHC430qiMP3Wadhcwje0Pk2Qoc4JV3XptqLi900jCY7tfas',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
